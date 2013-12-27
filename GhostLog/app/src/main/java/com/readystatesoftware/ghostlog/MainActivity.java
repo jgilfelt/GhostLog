49d2ca86b345fd1a98740454566220ab41b5ba8a
jgilfelt <jgilfelt@gmail.com>
2013-12-27 14:53:34 +0000

    prefs implementation

--- GhostLog/app/src/main/java/com/readystatesoftware/ghostlog/MainActivity.java
+++ GhostLog/app/src/main/java/com/readystatesoftware/ghostlog/MainActivity.java
@@ -3,9 +3,12 @@ package com.readystatesoftware.ghostlog;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 import java.util.List;
 
@@ -49,31 +52,68 @@ public class MainActivity extends PreferenceActivity {
         }
 
         addPreferencesFromResource(R.xml.pref_blank);
+
         // Add 'filters' preferences.
         PreferenceCategory fakeHeader = new PreferenceCategory(this);
         fakeHeader.setTitle(R.string.filters);
         getPreferenceScreen().addPreference(fakeHeader);
         addPreferencesFromResource(R.xml.pref_filters);
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_log_level)));
 
         // Add 'appearance' preferences.
         fakeHeader = new PreferenceCategory(this);
         fakeHeader.setTitle(R.string.appearance);
         getPreferenceScreen().addPreference(fakeHeader);
-        addPreferencesFromResource(R.xml.pref_filters);
+        addPreferencesFromResource(R.xml.pref_appearance);
 
     }
 
     /**
-     * A placeholder fragment containing a simple view.
+     * A preference value change listener that updates the preference's summary
+     * to reflect its new value.
      */
-    public static class PlaceholderFragment extends PreferenceFragment {
-
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
         @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.pref_filters);
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list.
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+                // Set the summary to reflect the new value.
+                preference
+                        .setSummary(index >= 0 ? listPreference.getEntries()[index]
+                                : null);
+            } else {
+                // For all other preferences, set the summary to the value's
+                // simple string representation.
+                preference.setSummary(stringValue);
+            }
+            return true;
         }
+    };
+
+    /**
+     * Binds a preference's summary to its value. More specifically, when the
+     * preference's value is changed, its summary (line of text below the
+     * preference title) is updated to reflect the value. The summary is also
+     * immediately updated upon calling this method. The exact display format is
+     * dependent on the type of preference.
+     *
+     * @see #sBindPreferenceSummaryToValueListener
+     */
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference
+                .setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+        // Trigger the listener immediately with the preference's
+        // current value.
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(
+                preference,
+                PreferenceManager.getDefaultSharedPreferences(
+                        preference.getContext()).getString(preference.getKey(),
+                        ""));
     }
 
     /**
@@ -94,6 +134,25 @@ public class MainActivity extends PreferenceActivity {
      */
     private static boolean isSimplePreferences(Context context) {
         return ALWAYS_SIMPLE_PREFS || !isXLargeTablet(context);
+    }
+
+    public static class FilterPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.pref_filters);
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_log_level)));
+        }
+    }
+
+    public static class AppearancePreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.pref_appearance);
+        }
     }
 
 }
