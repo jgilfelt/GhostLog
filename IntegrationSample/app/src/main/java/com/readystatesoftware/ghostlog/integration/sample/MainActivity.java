bd506dc86a08b5245a3d6f86ee91e29d1dc99c80
jgilfelt <jgilfelt@gmail.com>
2013-12-30 17:16:50 +0000

    add integration broadcast add integration sample

--- IntegrationSample/app/src/main/java/com/readystatesoftware/ghostlog/integration/sample/MainActivity.java
+++ IntegrationSample/app/src/main/java/com/readystatesoftware/ghostlog/integration/sample/MainActivity.java
@@ -1,19 +1,19 @@
 package com.readystatesoftware.ghostlog.integration.sample;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-
-import com.readystatesoftware.ghostlog.integration.IntegrationReceiver;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private static final String TAG_HELLO = "Hello";
+    private static final String TAG_SPAM = "Spam";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,43 +22,83 @@ public class MainActivity extends Activity {
 
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new MainFragment())
                     .commit();
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    public static class MainFragment extends Fragment {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+        private MySpamTask task;
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
+        public MainFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            Button button = (Button) rootView.findViewById(R.id.btn_hello);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.v(TAG_HELLO, "verbose message");
+                    Log.d(TAG_HELLO, "debug message");
+                    Log.i(TAG_HELLO, "info message");
+                    Log.w(TAG_HELLO, "warning message");
+                    Log.e(TAG_HELLO, "error message");
+                    Log.wtf(TAG_HELLO, "wtf message");
+                }
+            });
+
+            button = (Button) rootView.findViewById(R.id.btn_spam);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (task == null) {
+                        task = new MySpamTask();
+                        task.execute();
+                        ((Button) v).setText(R.string.stop_spam);
+                    } else {
+                        task.cancel(true);
+                        task = null;
+                        ((Button) v).setText(R.string.start_spam);
+                    }
+                }
+            });
+
             return rootView;
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            if (task != null) {
+                task.cancel(true);
+            }
+        }
+    }
+
+    public static class MySpamTask extends AsyncTask<Void, Integer, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... arg0) {
+            Log.d(TAG_SPAM, "start spam");
+            while (true) {
+                if (isCancelled()) {
+                    return true;
+                }
+                int random = (int)(Math.random() * ((Cheeses.sCheeseStrings.length-1) + 1));
+                Log.i(TAG_SPAM, Cheeses.sCheeseStrings[random]);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+        @Override
+        protected void onCancelled(Boolean result) {
+            Log.d(TAG_SPAM, "stop spam");
         }
     }
 
