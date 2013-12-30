e1a7c639795e7fa2c18dc4b2a7e018d7e01d8ef6
jgilfelt <jgilfelt@gmail.com>
2013-12-30 15:33:52 +0000

    add integration broadcast

---
+++ GhostLog/app/src/main/java/com/readystatesoftware/ghostlog/integration/Constants.java
@@ -0,0 +1,7 @@
+package com.readystatesoftware.ghostlog.integration;
+
+public class Constants {
+    public static final String ACTION_COMMAND = "com.readystatesoftware.ghostlog.integration.COMMAND";
+    public static final String EXTRA_ENABLED = "com.readystatesoftware.ghostlog.integration.EXTRA_ENABLED";
+    public static final String ACTION_LOG = "com.readystatesoftware.ghostlog.integration.LOG";
+}
