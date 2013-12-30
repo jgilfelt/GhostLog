f5cced33b5925471abc953059a5509c7f175de8e
jgilfelt <jgilfelt@gmail.com>
2013-12-30 22:16:24 +0000

    remove old integration sample

--- IntegrationSample/app/src/main/java/com/readystatesoftware/ghostlog/integration/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.readystatesoftware.ghostlog.integration;
-
-public class Constants {
-    public static final String ACTION_COMMAND = "com.readystatesoftware.ghostlog.integration.COMMAND";
-    public static final String EXTRA_ENABLED = "com.readystatesoftware.ghostlog.integration.EXTRA_ENABLED";
-    public static final String ACTION_LOG = "com.readystatesoftware.ghostlog.integration.LOG";
-    public static final String EXTRA_LINE = "com.readystatesoftware.ghostlog.integration.EXTRA_LINE";
-}
